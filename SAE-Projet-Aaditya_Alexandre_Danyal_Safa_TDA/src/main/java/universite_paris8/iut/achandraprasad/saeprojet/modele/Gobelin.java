d077422be5a95525178e5c8fefb503c4e0a69f5b
Dakoumc <danyalchudhary@gmail.com>
2025-06-12 20:20:14 +0200

    Add files via upload

---
+++ SAE-Projet-Aaditya_Alexandre_Danyal_Safa_TDA/src/main/java/universite_paris8/iut/achandraprasad/saeprojet/modele/Gobelin.java
@@ -0,0 +1,12 @@
+package universite_paris8.iut.achandraprasad.saeprojet.modele;
+
+public class Gobelin extends Ennemie {
+
+
+    public Gobelin(Terrain terrain, double x, double y) {
+
+        super(terrain, x, y);
+
+    }
+
+}
